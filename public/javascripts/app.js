104bea38a6e57a9dfc28cd40632d792b1143333c
Forrest Held <forrest.held@comcast.net>
2015-04-18 19:42:41 +0000

    Added a profile and subscription page with jquery tabs

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -269,6 +269,7 @@ var changeAlbumView = function(album) {
    require("./landing");
       require('./collection');
       require('./album');
+        require("./profile");
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
@@ -360,6 +361,29 @@ var changeAlbumView = function(album) {
     $('.selling-points .point').hover(onHoverAction, offHoverAction);
   });
   
+});
+
+;require.register("scripts/profile", function(exports, require, module) {
+ var tabsContainer = ".user-profile-tabs-container"
+ var selectTabHandler = function(event) {
+   $tab = $(this);
+   $(tabsContainer + " li").removeClass('active');
+   $tab.parent().addClass('active');
+   selectedTabName = $tab.attr('href');
+   console.log(selectedTabName);
+   $(".tab-pane").addClass('hidden');
+   $(selectedTabName).removeClass('hidden');
+   event.preventDefault();
+ };
+
+ 
+ if (document.URL.match(/\/profile.html/)) {
+   $(document).ready(function() {
+     var $tabs = $(tabsContainer + " a");
+     $tabs.click(selectTabHandler);
+     $tabs[0].click();
+   });
+ }
 });
 
 ;
